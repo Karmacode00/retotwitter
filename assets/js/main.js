37402752569e2a404a552a34af2c9d1b5f01349e
AndreaL <alafertteo@gmail.com>
2018-06-12 13:04:47 -0400

    Version dos, falta bloquear boton

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,16 @@
+let btn
+
+
 const boton = document.getElementById("btn");
+
 boton.addEventListener("click", () => {
+
+
+
   //Acá se guarda el comentario ingresado por el usuario
   let comments = document.getElementById("comment").value;
-
+  
+  
   //Limpiar el textarea
   document.getElementById("comment").value = "";
 
@@ -11,6 +19,12 @@ boton.addEventListener("click", () => {
 
   //Crear div contenedor
   const newComments = document.createElement("div");
+
+//Textarea tenga un comentario
+/* if(comments.length === 0 || comments === null) {
+  alert("Debes ingresar un mensaje");
+  return false;
+} */
 
 
 //Nodos de texto del textarea
@@ -23,4 +37,25 @@ newComments.appendChild(contenedorElemento);
 
 cont.appendChild(newComments);
 
+
 })
+
+function contar() {
+  const max = "140";
+  let cadena = document.getElementById("comment").value;
+  let longitud = cadena.length;
+ 
+    if(longitud <= max) {
+         document.getElementById("contador").value = max-longitud;
+    } else {
+         document.getElementById("textArea").value = cadena.substr(0, max);
+    }
+ }
+
+ /* function check() {
+  if(document.getElementById("comment").value==="") { 
+         document.getElementById('comment').disabled = true; 
+     } else { 
+         document.getElementById('comment').disabled = false;
+     }
+ } */
